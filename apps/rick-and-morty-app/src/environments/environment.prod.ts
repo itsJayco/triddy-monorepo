798b90a75011f048fa4a8c2869d5662d998dc4dc
Jacob <jacobo.jimenez@n5now.com>
2024-09-18 13:09:23 -0500

    route

--- apps/rick-and-morty-app/src/environments/environment.prod.ts
+++ apps/rick-and-morty-app/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://rickandmortyapi.com/api',
+  baseHref: '/triddy-monorepo/'
 };
--- apps/rick-and-morty-app/src/main.ts
+++ apps/rick-and-morty-app/src/main.ts
@@ -3,6 +3,14 @@ import { provideRouter } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
 import { AppComponent } from './app/app.component';
 import { routes } from './app/app.routes';
+import { environment } from './environments/environment';
+
+window.__env = {
+  baseHref: environment.baseHref
+};
+
+window.__env = window.__env || {};
+window.__env.baseHref = environment.baseHref || '/';
 
 bootstrapApplication(AppComponent, {
   providers: [
